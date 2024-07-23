ba416e30f844d441d5cffdf81370dbca32392420
Eduardo Narez <eduardomoralesnarez@gmail.com>
2024-07-23 17:48:53 -0600

    slide

--- web/views/pages/home/modules/slide.php
+++ web/views/pages/home/modules/slide.php
@@ -12,7 +12,7 @@
                         <h2 class="text-uppercase" style="color:#777">Lorem ipsum dolor s</h2>
                         <h3 class="text-uppercase" style="color:#888">Lorem ipsum dolor s</h3>
                         <a href="#">
-                            <button class="my-lg-3 btn text-uppercase template border-0">
+                            <button class="my-lg-3 btn text-uppercase templateColor border-0">
                                 VER PRODUCTO
                             </button>
                         </a>
@@ -22,13 +22,13 @@
             <li>
                 <img src="<?php echo $path ?>views/assets/img/slide/2/fondo2.jpg" class="img-fluid">
                 <div class="slideOpt opt2">
-                <img src="<?php echo $path ?>views/assets/img/slide/2/iphone.png" style="bottom:0%; left:15%; width: 28%;">
-                    <div class="slideText" style="top:20%; left:15%; width:40%;">
+                    <img src="<?php echo $path ?>views/assets/img/slide/2/iphone.png" style="bottom:0%; left:15%; width: 28%">
+                    <div class="slideText" style="top:20%; left:15%; width:70%">
                         <h1 class="text-uppercase" style="color:#fff">Lorem ipsum</h1>
                         <h2 class="text-uppercase" style="color:#fff">Lorem ipsum dolor s</h2>
                         <h3 class="text-uppercase" style="color:#fff">Lorem ipsum dolor s</h3>
                         <a href="#">
-                            <button class="my-lg-3 btn text-uppercase template border-0">
+                            <button class="my-lg-3 btn text-uppercase templateColor border-0">
                                 VER PRODUCTO
                             </button>
                         </a>
