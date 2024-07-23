56121bc2fce34524de37d1c9e2d64b051cddb03f
Eduardo Narez <eduardomoralesnarez@gmail.com>
2024-07-23 16:00:53 -0600

    slide

--- web/views/pages/home/modules/slide.php
+++ web/views/pages/home/modules/slide.php
@@ -5,9 +5,35 @@
         <ul class="slide-area">
             <li>
                 <img src="<?php echo $path ?>views/assets/img/slide/1/back_default.jpg" class="img-fluid">
+                <div class="slideOpt opt1">
+                    <img src="<?php echo $path ?>views/assets/img/slide/1/calzado.png" style="top:15%; right:10%; width: 45%;">
+                    <div class="slideText" style="top: 20%; left: 10%; width:40%">
+                        <h1 class="text-uppercase" style="color:#333">Lorem ipsum</h1>
+                        <h2 class="text-uppercase" style="color:#777">Lorem ipsum dolor s</h2>
+                        <h3 class="text-uppercase" style="color:#888">Lorem ipsum dolor s</h3>
+                        <a href="#">
+                            <button class="my-lg-3 btn text-uppercase template border-0">
+                                VER PRODUCTO
+                            </button>
+                        </a>
+                    </div>
+                </div>
             </li>
             <li>
                 <img src="<?php echo $path ?>views/assets/img/slide/2/fondo2.jpg" class="img-fluid">
+                <div class="slideOpt opt2">
+                <img src="<?php echo $path ?>views/assets/img/slide/2/iphone.png" style="bottom:0%; left:15%; width: 28%;">
+                    <div class="slideText" style="top:20%; left:15%; width:40%;">
+                        <h1 class="text-uppercase" style="color:#fff">Lorem ipsum</h1>
+                        <h2 class="text-uppercase" style="color:#fff">Lorem ipsum dolor s</h2>
+                        <h3 class="text-uppercase" style="color:#fff">Lorem ipsum dolor s</h3>
+                        <a href="#">
+                            <button class="my-lg-3 btn text-uppercase template border-0">
+                                VER PRODUCTO
+                            </button>
+                        </a>
+                    </div>
+                </div>
             </li>
             <li>
                 <img src="<?php echo $path ?>views/assets/img/slide/3/fondo3.jpg" class="img-fluid">
@@ -27,4 +53,4 @@
     </div>
 </div>
 
-<script> src="<?php echo $path ?>views/assets/js/jdSlide/jdSlide.js"></script>
+<script src="<?php echo $path ?>views/assets/js/slide/slide.js"></script>
