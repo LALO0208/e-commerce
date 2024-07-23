c51e9f62aa8a04aabc9d8c2cc616efe68b86e9c7
Eduardo Narez <eduardomoralesnarez@gmail.com>
2024-07-23 13:22:30 -0600

    Slide

---
+++ web/views/pages/home/modules/slide.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="<?php echo $path ?>views/assets/css/slide/slide.css">
+
+<div class="jd-slider">
+    <div class="slide-inner">
+        <ul class="slide-area">
+            <li>
+                <img src="<?php echo $path ?>views/assets/img/slide/1/back_default.jpg" class="img-fluid">
+            </li>
+            <li>
+                <img src="<?php echo $path ?>views/assets/img/slide/2/fondo2.jpg" class="img-fluid">
+            </li>
+            <li>
+                <img src="<?php echo $path ?>views/assets/img/slide/3/fondo3.jpg" class="img-fluid">
+            </li>
+        </ul>
+    </div>
+    <a class="prev" href="#">
+        <i class="fas fa-angle-left text-white px-3 py-4 rounded-right" style="background: rgba(0,0,0,.5)"></i>
+    </a>
+    <a class="next" href="#">
+        <i class="fas fa-angle-right text-white px-3 py-4 rounded-left" style="background: rgba(0,0,0,.5)"></i>
+    </a>
+    <div class="controller">
+        <div class="indicate-area">
+
+        </div>
+    </div>
+</div>
+
+<script> src="<?php echo $path ?>views/assets/js/jdSlide/jdSlide.js"></script>
