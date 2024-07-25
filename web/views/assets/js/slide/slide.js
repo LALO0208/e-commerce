00062909cde4070b888b6a5f67bb705d68a16357
Eduardo Narez <eduardomoralesnarez@gmail.com>
2024-07-24 21:42:17 -0600

    Slide mobile

--- web/views/assets/js/slide/slide.js
+++ web/views/assets/js/slide/slide.js
@@ -1,1 +1,21 @@
-$('.jd-slider').jdSlider();
+$('.jd-slider').jdSlider({
+    isLoop:true,
+    speed:500,
+});
+
+/*btn slide*/
+var toogle = false;
+
+$(document).on("click","#btnSlide",function(){
+
+    if(!toogle){
+        $('.jd-slider').slideUp("fast");
+        $('#btnSlide').html('<i class="fa fa-angle-down templateColor"></i>')
+        toogle = true;
+    }else{
+        $('.jd-slider').slideDown("fast");
+        $('#btnSlide').html('<i class="fa fa-angle-up templateColor"></i>')
+        toogle = false;
+    }
+
+})
