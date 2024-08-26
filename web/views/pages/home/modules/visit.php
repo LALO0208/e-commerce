a39bfc26a7119bb93776a86529a5222004f98c73
Eduardo Narez <eduardomoralesnarez@gmail.com>
2024-08-25 20:28:06 -0600

    modulo visits

---
+++ web/views/pages/home/modules/visit.php
@@ -0,0 +1,42 @@
+<div class="container-fluid bg-light border">
+    <div class="container clearfix">
+        <div class="btn-group float-end p-2">
+            <p class="pt-3 lead">Tu eres nuestro visitante #0000</p>
+        </div>
+    </div>
+</div>
+<div class="container py-3 pb-5">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-6">
+        <div class="col text-center">
+            <h3 class="py-3 text-muted">Colombia</h3>
+            <input type="text" class="knob" value="70" data-width="90" data-height="90" data-fgColor="#3c8dbc">
+            <div class="knob-label">70% de las visitas</div>
+        </div>
+        <div class="col text-center">
+            <h3 class="py-3 text-muted">México</h3>
+            <input type="text" class="knob" value="50" data-width="90" data-height="90" data-fgColor="#00C0EF">
+            <div class="knob-label">50% de las visitas</div>
+        </div>
+        <div class="col text-center">
+            <h3 class="py-3 text-muted">Perú</h3>
+            <input type="text" class="knob" value="25" data-width="90" data-height="90" data-fgColor="#39CCCC">
+            <div class="knob-label">25% de las visitas</div>
+        </div>
+        <div class="col text-center">
+            <h3 class="py-3 text-muted">España</h3>
+            <input type="text" class="knob" value="15" data-width="90" data-height="90" data-fgColor="#00A65A">
+            <div class="knob-label">15% de las visitas</div>
+        </div>
+        <div class="col text-center">
+            <h3 class="py-3 text-muted">Argentina</h3>
+            <input type="text" class="knob" value="15" data-width="90" data-height="90" data-fgColor="#F56954">
+            <div class="knob-label">15% de las visitas</div>
+        </div>
+        <div class="col text-center">
+            <h3 class="py-3 text-muted">Ecuador</h3>
+            <input type="text" class="knob" value="15" data-width="90" data-height="90" data-fgColor="#932AB6">
+            <div class="knob-label">15% de las visitas</div>
+        </div>
+    </div>
+</div>
+<script src="<?php echo $path ?>views/assets/js/visits/visits.js"> </script>
